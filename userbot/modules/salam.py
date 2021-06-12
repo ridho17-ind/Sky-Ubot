3224b6814de063f5a1a0d37b9938536a1efb8a2c
ridho17-ind <81844427+ridho17-ind@users.noreply.github.com>
2021-06-12 15:57:24 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -9,7 +9,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**Hallo Gaes Gua Skyzo🧸...**")
+    await typew.edit("**Hallo Gaes Gua {DEFAULTUSER}🧸...**")
 
 
 @register(outgoing=True, pattern='^.atg(?: |$)(.*)')
@@ -21,7 +21,7 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**Yoi Bro Salken Juga🗿...**")
+    await typew.edit("**Yoi Bro Salken Juga Nama Gua {DEFAULTUSER}🗿...**")
 
 
 @register(outgoing=True, pattern='^.ast(?: |$)(.*)')
