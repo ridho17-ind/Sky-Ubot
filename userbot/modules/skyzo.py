67306f18f9a4019ac763d623990f727dfe038378
ridho17-ind <81844427+ridho17-ind@users.noreply.github.com>
2021-06-12 13:42:26 +0700

    Update skyzo.py

--- userbot/modules/skyzo.py
+++ userbot/modules/skyzo.py
@@ -1,3 +1,50 @@
+Skip to content
+ridho17-ind
+/
+Sky-Ubot
+forked from ramadhani892/RAM-UBOT
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Sky-Ubot/skyzo.py
+@ridho17-ind
+ridho17-ind Update and rename userbot/modules/ramaa.py to skyzo.py
+ 1 contributor
+70 lines (62 sloc)  2.1 KB
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.sadboy(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("`Pertama-tama kamu cantik`")
+    sleep(2)
+    await typew.edit("`Kedua kamu manis`")
+    sleep(1)
+    await typew.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.punten(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n┻┳|―-∩`"
+                     "`\n┳┻|     ヽ`"
+                     "`\n┻┳|    ● |`"
+                     "`\n┳┻|▼) _ノ`"
+                     "`\n┻┳|￣  )`"
+                     "`\n┳ﾐ(￣ ／`"
+                     "`\n┻┳T￣|`"
+                     "\n**Permisi Aku mau nimbrung Kk..**")
+
 
 @register(outgoing=True, pattern='^.geez(?: |$)(.*)')
 async def typewriter(typew):
@@ -27,3 +74,28 @@ async def typewriter(typew):
     await typew.edit("**Afreal,Mengintil✅**")
     sleep(3)
     await typew.edit("**CUMA SKYZO YANG BENER!**")
+
+# Create by myself @localheart
+
+CMD_HELP.update({
+    "rambot":
+    "`.rambot`\
+    \nUsage: menampilkan alive bot.\
+    \n\n`.sadboy`\
+    \nUsage: hiks\
+    \n\n`.punten` ; `.geez`\
+    \nUsage: misi."
+})
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
