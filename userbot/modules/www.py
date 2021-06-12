3927c54b509cf7608d2a6900e660457736f7f35d
ridho17-ind <81844427+ridho17-ind@users.noreply.github.com>
2021-06-12 10:00:54 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -147,11 +147,11 @@ async def redis(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("NGENTOT!")
+    await pong.edit("💸")
     await asyncio.sleep(1)
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**KONTOOLLLL!!**\n**KEKUATAN KONTOL** : `%sms`\n**DURASI KONTOL** : `{uptime}🕛`" % (duration))
+    await pong.edit(f"**Hai Sayang🐊!!**\n**Ping Bot** : `%sms`\n**Durasi Bot** : `{uptime}🕛`" % (duration))
 
 
 @register(outgoing=True, pattern="^Speed$")
